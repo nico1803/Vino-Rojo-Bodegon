47b9e6419402c101946f54ed89c3015425a2dd04
Nicolas Beovide <nico1803o@hotmail.com.ar>
2023-01-18 19:37:59 -0300

    Feat (components) card-cards-filters-searchbar + changes home/navbar

---
+++ client/src/components/Detail.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ProductoImage from '../assets/PIDEA.jpg'
+import '../styles/detail.css'
+
+
+export default function Detail() {
+    return (<>
+
+    <div className="ProductPrinInfo">
+    <img src={ProductoImage} alt="Product Image" />
+    <h1>NOMBRE DEL PRODUCTO</h1>
+    <p>Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseo visual antes de insertar el texto final.</p>
+    <h2>$100</h2>
+    <button>AADIR</button>
+    <button>⭐</button>
+    </div>
+
+    </>)
+  }
+  
+
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,53 @@ import '../styles/navbar.css'
 
 export default function Navbar() {
   return (
-    <nav>
-        <img src={logoImg} alt="" />
-        <p>Hola</p>
-    </nav>
+    <header>
+      <div className="container">
+        <div className="nav__wrapper">
+          <div className="logo">
+            <img src={logoImg} alt="Logo image" />
+            <h2>Vino Rojo Bodegon</h2>
+          </div>
+          {/* ========= navigation =========*/}
+          <div className="navigation">
+              <ul className="menu">
+                        <li className="menu__item">
+                          <a className="menu__link"></a>
+                          </li>
+              </ul>
+          </div>
+
+        </div>
+      </div>
+    </header>
   )
 }
 
+
+
+// <header className="header">
+// <div className="container">
+//     <div className="nav__wrapper">
+//         <div className="logo">
+//             <h2>Digency</h2>
+//         </div>
+
+//         /* ========= navigation =========*/
+//         <div className="navigation">
+//             <ul className="menu">
+//                 {
+//                     nav_links.map((item, index)=>(
+//                         <li className="menu__item"><a href={item.path} className="menu__link">{item.display}</a></li>
+//                     ))
+//                 }
+//             </ul>
+//         </div>
+    
+//         /* ========= ligth mode =========*/
+//         <div className="light__mode">
+//                 <span><i class="ri-sun-line"></i> Ligth Mode 
+//                 </span>
+//         </div>
+//     </div>
+// </div>
+// </header>
