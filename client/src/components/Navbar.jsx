70758df9d210adba1066203853f1137ade99b9ae
Naanita <sebastian.grajales15@gmail.com>
2023-01-19 14:14:53 -0500

    Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,30 +1,78 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logoImg from '../assets/LogoBarril.png'
 import '../styles/navbar.css'
 
 export default function Navbar() {
   return (
-    <header>
-      <div className="container">
-        <div className="nav__wrapper">
-          <div className="logo">
-            <img src={logoImg} alt="Logo image" />
-            <h2>Vino Rojo Bodegon</h2>
-          </div>
-          {/* ========= navigation =========*/}
-          <div className="navigation">
-              <ul className="menu">
-                        <li className="menu__item">
-                          <a className="menu__link"></a>
-                          </li>
-              </ul>
-          </div>
+<section class="navigation">
+  <div class="nav-container">
+    <div class="brand">
+      <Link to="/"><img className='imga' src={logoImg}  alt="Logo image" /></Link>
+    </div>
+    <nav>
+      <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+      <ul class="nav-list">
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">About</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Services</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Pricing</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Portfolio</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</section>
 
-        </div>
-      </div>
-    </header>
   )
 }
+<section class="navigation">
+  <div class="nav-container">
+    <div class="brand">
+      <Link to="/"><img className='imga' src={logoImg}  alt="Logo image" /></Link>
+    </div>
+    <nav>
+      <div class="nav-mobile"><a id="nav-toggle" href="#!"><span></span></a></div>
+      <ul class="nav-list">
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">About</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Services</a>
+          <ul class="nav-dropdown">
+            <li>
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">s1</a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">s2</a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">s3</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Pricing</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Portfolio</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</section>
 
 
 
