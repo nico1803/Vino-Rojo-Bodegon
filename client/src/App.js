f291368fbd9fd26aa31364cab1d9fce923299840
Nicolas Beovide <nico1803o@hotmail.com.ar>
2023-01-18 23:08:35 -0300

    Feat (Router)

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Home from './components/Home';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <Home/>
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+    </Routes>
   );
 }
 
