7d9edb1d57b8984545a1f3481004323adc3f8299
Naanita <sebastian.grajales15@gmail.com>
2023-01-20 18:39:48 -0500

    logincard

--- client/src/components/Loginpage.jsx
+++ client/src/components/Loginpage.jsx
@@ -6,21 +6,14 @@ import { Link } from "react-router-dom";
 // href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="blank"
 export default function Login() {
   return (
-    <div class="cardg">
-  <div class="card-info">
-  <div class='wrapper' >  
-  <span class='content img' >
-   <img src="https://madeby.google.com/static/images/google_g_logo.svg" />
-  </span>
-  <span class='content sign-text'>
-    <Link to="/">
-    <a>Sign In with Google</a>
-    </Link>
-  </span>
+<div class="cardlogin">
+<div class="cardg-img">
+<img src={LogGoogle}/>
 </div>
+  <div class="cardg-info">
+    <p class="text-body">Te invitamos ser parte registrarte atravez de google para tu comodidad.</p>
+    <a class="text-title">Autor</a>
   </div>
 </div>
-
-
   )
   }
