69a6237a9fefaa614ff75d2864f814af7ae58891
Nicolas Beovide <nico1803o@hotmail.com.ar>
2023-01-20 19:46:13 -0300

    Feat (components / states) Card,Home,Filters,Sarchbar,Detail,Navbar

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar';
 
+//name, image, description, price, review, type, _id
+export default function Card({food}) {
 
-export default function Card({name, image, description, price, review, type, _id}) {
     return (
-        <div className='grid grid-cols-1 bg-white rounded-md'>
-            <img className='rounded-md rounded-b-none' src={image} alt={name}/>
-            <div className='p-4'>
-                <h2 className=''>{name}</h2>
+        
+    <div className="card lg:card-side bg-base-100 shadow-xl">
+            <figure><img className='w-[400px] h-[300px]' src={food.image}/></figure>
+        <div className="flex justify-end">
+            <h2 className="card-title">{food.name}</h2>
+            <p>{food.description}</p>
+            <p>{food.price}</p>
+            <div className="card-actions justify-end">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Comprar</button>
             </div>
         </div>
+    </div>
     )
 }
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -6,17 +6,19 @@ export default function SearchBar() {
     const [input, setInput] = useState("");
 
     let handleClick = (event) => {
-
+        event.preventDefault();
     }
-
     return (
         <div>
-
-            <div>
-                <button type="submit" onClick={event => handleClick(event)} >Buscar</button>
-                <input type="search" placeholder="Buscar plato" value={input} onChange={null}> </input>
+            <div class="flex-row rounded-2xl border-0 bg-stone-900 scroll-p-3">
+                <button type="submit" onClick={event => handleClick(event)} class="rounded-l-lg text-base basis-1  p-0.5 bg-stone-500 hover:bg-stone-700 border-0">Buscar</button>
+                <input type="text" name="Buscar plato" id="" class="rounded-r-lg p-0.5 bg-stone-200 border-0 caret-red-400"/>
             </div>
-
         </div>
     )
 }
+
+
+//<form className='Form' onSubmit={(e)=>handleSubmit(e)}>
+//<input className='SearchBar' placeholder='Search by breed...' type='text' name='input'onChange={(e)=>{handleChange(e)}}/>    
+//</form> 
