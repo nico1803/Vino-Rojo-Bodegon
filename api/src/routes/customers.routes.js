7e1290ce8dafb673c0d68a781d27e6412c6cd0c1
Lauti Posti <lautaropostiglione@gmail.com>
2023-01-24 17:05:03 -0300

    Terminadas rutas de customers

--- api/src/routes/customers.routes.js
+++ api/src/routes/customers.routes.js
@@ -1,5 +1,6 @@
 const {Router} = require('express');
-const {createCustomer,getCustomers, emailValidation} = require('../controllers/index');
+const {createCustomer,getCustomers, emailValidation, deleteCustomer, updateCustomer} = require('../controllers/index');
+const Customer = require('../models/customers');
 
 const router = Router();
 
@@ -13,17 +14,48 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.get('/', async (req, res) => {
-    const {email} = req.body;
+router.get('/customers', async (req, res) => {
     try {
-        let validation = await emailValidation(email);
-        if(validation === true){
-            return res.status(400).send('Este email ya está en uso');
-        };
-        res.status(200).send('El mail se puede usar');
+        const customers = await getCustomers();
+        res.send(customers);
     } catch (error) {
-        res.status(400).send(error)
-    } 
+        res.send(error);
+    }
+})
+
+router.get('/:email', async (req, res) => {
+    const {email} = req.params;
+    try {
+        let validate = await emailValidation(email);
+        if(validate == true){
+            return res.status(400).send('Este email ya está en uso');
+        } else if (validate == false) {
+            return res.send('El mail se puede utilizar')
+        }
+    } catch (error) {
+        res.send(error);
+    }
 }); 
+
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        await deleteCustomer(id);
+        res.status(200).send('El usuario fue eliminado');
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const {id} = req.params;
+    const {name, email, password} = req.body    
+    try {
+        await updateCustomer(id, name, password, email);
+        res.status(200).send('La constraseña fue actualizada');
+    } catch (error) {
+        res.send(error);
+    }
+});
 
 module.exports = router;
