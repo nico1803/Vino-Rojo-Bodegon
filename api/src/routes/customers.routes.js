59ba9ee4a7ae109dc66b26bb2c51cfdd443adcae
EstefaniaDelgado <teff_64@hotmail.com>
2023-01-25 17:29:39 -0500

    enviar informacion desde el formulario en el front al back para guardarla en la base de datos

--- api/src/routes/customers.routes.js
+++ api/src/routes/customers.routes.js
@@ -1,64 +1,69 @@
-const {Router} = require('express');
-const {createCustomer,getCustomers, emailValidation, deleteCustomer, updateCustomer} = require('../controllers/index');
+const { Router } = require('express');
+const {
+  createCustomer,
+  getCustomers,
+  emailValidation,
+  deleteCustomer,
+  updateCustomer,
+} = require('../controllers/index');
 const Customer = require('../models/customers');
 
 const router = Router();
 
 router.post('/', async (req, res) => {
-    const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
+  console.log(req.body);
+  try {
+    await createCustomer(name, email, password);
 
-    try {
-
-        await createCustomer(name, email, password);
-        
-        res.status(200).send('Usuario creado');
-    } catch (error) {
-        res.send(error);
-    }
+    res.status(200).send('Usuario creado');
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 router.get('/customers', async (req, res) => {
-    try {
-        const customers = await getCustomers();
-        res.send(customers);
-    } catch (error) {
-        res.send(error);
-    }
-})
+  try {
+    const customers = await getCustomers();
+    res.send(customers);
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 router.get('/:email', async (req, res) => {
-    const {email} = req.params;
-    try {
-        let validate = await emailValidation(email);
-        if(validate == true){
-            return res.status(400).send('Este email ya está en uso');
-        } else if (validate == false) {
-            return res.send('El mail se puede utilizar')
-        }
-    } catch (error) {
-        res.send(error);
+  const { email } = req.params;
+  try {
+    let validate = await emailValidation(email);
+    if (validate == true) {
+      return res.status(400).send('Este email ya está en uso');
+    } else if (validate == false) {
+      return res.send('El mail se puede utilizar');
     }
-}); 
+  } catch (error) {
+    res.send(error);
+  }
+});
 
 router.delete('/:id', async (req, res) => {
-    const {id} = req.params;
-    try {
-        await deleteCustomer(id);
-        res.status(200).send('El usuario fue eliminado');
-    } catch (error) {
-        res.status(400).send(error);
-    }
+  const { id } = req.params;
+  try {
+    await deleteCustomer(id);
+    res.status(200).send('El usuario fue eliminado');
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 router.put('/:id', async (req, res) => {
-    const {id} = req.params;
-    const {name, email, password} = req.body    
-    try {
-        await updateCustomer(id, name, password, email);
-        res.status(200).send('La constraseña fue actualizada');
-    } catch (error) {
-        res.send(error);
-    }
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+  try {
+    await updateCustomer(id, name, password, email);
+    res.status(200).send('La constraseña fue actualizada');
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -20,7 +20,7 @@ function SignUp() {
 
 //state para guardar el input del email y el password, y si hay mas input se añade a este objeto
 const [formData, setFormData]=useState({
-  username:"",
+  name:"",
   email:"",
   password:"",
   repeatpassword:"",
@@ -35,46 +35,45 @@ const handlerChange = (e)=>{
   console.log(formData)
 }
 
-//se puede hacer un state por cada input
-// const[email,setEmail]=useState("");
-// const[password, setPassword]=useState("");
 
-
-const handleSubmit = async e => {
-    e.preventDefault();
-try {
-  //envia la info de los inputs convertida a un json (formData)
-  const data = JSON.stringify(formData);
-  console.log(data)
-  //envio un fecth a la url del servidor que va a la ruta del post de customers con un objeto de configuracion donde le paso el metodo de la request, el body que contiene la data en formato json y un header para especificar que es un json el que estoy  enviando
-   await fetch("http://localhost:3001/login", {
-    method: "POST",
-    body: console.log(data),
-    headers: { "Content-Type": "application/json" }
-  });
-
-} catch (err) {
-  console.error(err);
-}
-  };
 
 
 //hacer nueva lidacion segun como se requieran los datos, dependiento de la forma de recibimieto de la db
-function handlesubmit(e) {
+ async function handlesubmit(e) {
   e.preventDefault();
-  if(!expusuario.test(formData.username)){return swal("UPS!", "Tu usuario debe contener más de 3 caracteres o no pasarte de los 15", "warning")}
+  try {
+    //envia la info de los inputs convertida a un json (formData)
+    const data = JSON.stringify(formData);
+    console.log(data)
+    //envio un fecth a la url del servidor que va a la ruta del post de customers con un objeto de configuracion donde le paso el metodo de la request, el body que contiene la data en formato json y un header para especificar que es un json el que estoy  enviando
+     await fetch("http://localhost:3001/login", {
+      method: "POST",
+      body: data,
+      headers: { "Content-Type": "application/json" }
+    });
+    
+    } catch (err) {
+    console.error(err);
+    }
+
+  
+  if(!expusuario.test(formData.name)){return swal("UPS!", "Tu usuario debe contener más de 3 caracteres o no pasarte de los 15", "warning")}
   if(!formData.email){return swal("UPS!", "¡Antes escribe tu email!", "warning")}
   if(!expcorreo.test(formData.email)){return swal ("UPS!", "Esto no parece un email.","warning" )}
   if(!formData.password){return swal("UPS", "Antes escribe tu contraseña!", "warning")}
   if(!expcontraseña.test(formData.password)){return swal ("UPS!", "Contraseña con al menos 8 caracteres, con al menos una letra minúscula, una mayúscula, un dígito y un caracter especial", "warning")}
 
 if(formData.password !== formData.repeatpassword){return swal("UPS!", "La contraseña no coincide", "error")}
-    if(formData.username && formData.email && formData.password === formData.repeatpassword ) {
+    if(formData.name && formData.email && formData.password === formData.repeatpassword ) {
       return swal("¡ESTUPENDO!", "Ahora inicia sesión!", "success") && history("/login")
-      } 
+      };
 
 
-}
+      
+
+};
+
+
 
 
 
@@ -98,7 +97,7 @@ if(formData.password !== formData.repeatpassword){return swal("UPS!", "La contra
               <h2>Login</h2>
 
               <div>
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={(e)=>handlesubmit(e)}>
                   <p>
                     <label>
                       Usuario<span>*</span>
@@ -107,8 +106,8 @@ if(formData.password !== formData.repeatpassword){return swal("UPS!", "La contra
                       type="text"
                       placeholder="Usuario"
                       required
-                      value={formData.username}
-                      name="username"
+                      value={formData.name}
+                      name="name"
                       onChange={handlerChange}
                     />
                   </p>
@@ -144,13 +143,13 @@ if(formData.password !== formData.repeatpassword){return swal("UPS!", "La contra
                     <input type="password"
                      placeholder="Repite tu contraseña" 
                      value={formData.repeatpassword}
-                     required 
+                     required
                      name="repeatpassword"
                      onChange={handlerChange}
                      />
                   </p>
                   <p>
-                    <input type="submit" id="submit" value="Crear" onClick={(e) => handlesubmit(e)} />
+                    <input type="submit" id="submit" value="Crear" /* onClick={(e) => handlesubmit(e)} */ />
                   </p>
                 </form>
               </div>
