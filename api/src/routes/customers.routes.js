62b7f8842ed4d066b0251c56d28d43ae78f50a64
Franco <francoherrera_8@hotmail.com.ar>
2023-01-25 17:35:49 -0300

    bcryptjs

--- api/src/routes/customers.routes.js
+++ api/src/routes/customers.routes.js
@@ -6,8 +6,11 @@ const router = Router();
 
 router.post('/', async (req, res) => {
     const {name, email, password} = req.body;
+
     try {
+
         await createCustomer(name, email, password);
+        
         res.status(200).send('Usuario creado');
     } catch (error) {
         res.send(error);
