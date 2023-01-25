e2c1c78c99f352a7ccb72c7517b73bb6c8768d22
dariorojas6 <dario43055@gmail.com>
2023-01-25 20:01:23 -0300

    arreglo a ruta drink

--- api/src/routes/api.js
+++ api/src/routes/api.js
@@ -1,7 +1,7 @@
 const {Router} = require('express')
 const router = Router()
 const foods = require('./foods.routes')
-const drinks = require('./drinks.routesjs')
+const drinks = require('./drinks.routes.js')
 const customers = require('./customers.routes')
 const filters = require ('./filters.routes');
 
