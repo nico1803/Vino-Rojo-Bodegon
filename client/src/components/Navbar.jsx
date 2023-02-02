2feca417c0bf5d7eb23ed24a5e401b0e4cbaf201
Constantino Abba <cons_benjamin9@outlook.com>
2023-02-02 19:21:07 -0300

    fix navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -67,11 +67,13 @@ export default function Navbar() {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
+                    <span className="md:hidden  pl-2"> Carrito</span>
                   </Link>
                   <Link className="flex items-center hover:text-gray-200" to={"/login"}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                     </svg>
+                    <span className="md:hidden  pl-2"> Login</span>
                   </Link>
                 </ul>
           </div>
