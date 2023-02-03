ba8d94b417471358962310c2bb4c6e48f7826bdd
Naanita <sebastian.grajales15@gmail.com>
2023-02-03 10:30:45 -0500

    about

--- client/src/components/Aboutus.jsx
+++ client/src/components/Aboutus.jsx
@@ -2,91 +2,163 @@ import "../styles/aboutus.css";
 
 export default function Aboutus() {
   return (
+    <>
+    <h1 className="TitleDevs">Dev's</h1>
+    <main className="page-content">
+      <div className="cardAbout">
+        <div className="content">
+          <h2 className="titleAbout">Sebastián Grajales</h2>
+          <p className="copy">De dia Programador, de noche, también.</p>
 
-      <main className="page-content">
-        <div className="cardAbout">
-          <div className="content">
-            <h2 className="titleAbout">Sebastián Grajales</h2>
-            <p className="copy">
-              De dia Programador, de noche, también.
-            </p>
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
+            <button className="btn1">LinkedIn</button>
+          </a>
 
-            <a href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/" target="blank"><button className="btn1" >LinkedIn</button></a>
+          <a
+            href="https://github.com/Naanita"
+            target="blank"
+          >
+            <button className="btn">Github</button>
+          </a>
+        </div>
+      </div>
+      <div className="cardAbout">
+        <div className="content">
+          <h2 className="titleAbout">Dario Rojas</h2>
+          <p className="copy">Description</p>
+          <a
+            href="https://www.linkedin.com/in/dario-rojas-991899251/"
+            target="blank"
+          >
+            <button className="btn1">LinkedIn</button>
+          </a>
 
-            <a href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/" target="blank"><button className="btn">Github</button></a>
-            
-          </div>
+          <a
+            href="https://github.com/dariorojas6"
+            target="blank"
+          >
+            <button className="btn">Github</button>
+          </a>
         </div>
-        <div className="cardAbout">
-          <div className="content">
-            <h2 className="titleAbout">Dario Rojas</h2>
-            <p className="copy">
-              Description
-            </p>
+      </div>
+      <div className="cardAbout">
+        <div className="content">
+          <h2 className="titleAbout">Nicolas Beovide</h2>
+          <p className="copy">Description</p>
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn1">LinkedIn</button>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn">Github</button>
-          </div>
+          </a>
         </div>
-        <div className="cardAbout">
-          <div className="content">
-            <h2 className="titleAbout">Nicolas Beovide</h2>
-            <p className="copy">
-              Description
-            </p>
+      </div>
+      <div className="cardAbout">
+        <div className="content">
+          <h2 className="titleAbout">Franco Herrera</h2>
+          <p className="copy">Description</p>
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn1">LinkedIn</button>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn">Github</button>
-          </div>
+          </a>
         </div>
-        <div className="cardAbout">
-          <div className="content">
-            <h2 className="titleAbout">Franco Herrera</h2>
-            <p className="copy">
-              Description
-            </p>
+      </div>
+      <div className="cardAbout">
+        <div className="content">
+          <h2 className="titleAbout">Estefania Delgado</h2>
+          <p className="copy">Description</p>
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn1">LinkedIn</button>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn">Github</button>
-          </div>
+          </a>
         </div>
-        <div className="cardAbout">
-          <div className="content">
-            <h2 className="titleAbout">Estefania Delgado</h2>
-            <p className="copy">
-              Description
-            </p>
-            <button classNamne="btn1">LinkedIn</button>
-            <button className="btn">Github</button>
-          </div>
-        </div>
-        <div className="cardAbout">
-          <div className="content">
-            <h2 className="titleAbout">Constantino Abba</h2>
-            <p className="copy">
-              Description
-            </p>
+      </div>
+      <div className="cardAbout">
+        <div className="content">
+          <h2 className="titleAbout">Constantino Abba</h2>
+          <p className="copy">Description</p>
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn1">LinkedIn</button>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn">Github</button>
-          </div>
+          </a>
         </div>
-        <div className="cardAbout">
-          <div className="content">
-            <h2 className="titleAbout">Ezequiel Correcher</h2>
-            <p className="copy">
-              Description
-            </p>
+      </div>
+      <div className="cardAbout">
+        <div className="content">
+          <h2 className="titleAbout">Ezequiel Correcher</h2>
+          <p className="copy">Description</p>
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn1">LinkedIn</button>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn">Github</button>
-          </div>
+          </a>
         </div>
-        <div className="cardAbout">
-          <div className="content">
-            <h2 className="titleAbout">Lautaro Postiglione</h2>
-            <p className="copy">
-              Description
-            </p>
+      </div>
+      <div className="cardAbout">
+        <div className="content">
+          <h2 className="titleAbout">Lautaro Postiglione</h2>
+          <p className="copy">Description</p>
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn1">LinkedIn</button>
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/sebasti%C3%A1n-grajales-morales-14a277212/"
+            target="blank"
+          >
             <button className="btn">Github</button>
-          </div>
+          </a>
         </div>
-      </main>
+      </div>
+    </main>
+    </>
   );
 }
