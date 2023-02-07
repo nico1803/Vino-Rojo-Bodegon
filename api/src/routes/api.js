2f70315055ba490c409e7a85f2e22c3f69965405
dariorojas6 <dario43055@gmail.com>
2023-02-06 21:20:15 -0300

    filters drinks

--- api/src/routes/api.js
+++ api/src/routes/api.js
@@ -2,13 +2,11 @@ const {Router} = require('express');
 require('dotenv').config();
 const router = Router();
 const foods = require('./foods.routes');
-const drinks = require('./drinks.routes.js');
 const customers = require('./customers.routes');
 const filters = require ('./filters.routes');
 const email = require("./email.routes");
 
 router.use('/foods', foods);
-router.use('/drinks', drinks);
 router.use('/login', customers);
 router.use('/filters', filters);
 router.use("/email", email);
