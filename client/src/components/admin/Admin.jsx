bd0940252d95c3e73264ac0cb7b782ee781ed887
Nicolas Beovide <nico1803o@hotmail.com.ar>
2023-02-07 20:32:06 -0300

    feat (admin) fix

--- client/src/components/admin/Admin.jsx
+++ client/src/components/admin/Admin.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CardAdmin from './CardAdmin';
 import { getFoods } from "../../redux/actions";
+import axios from 'axios';
 
 
 
@@ -19,16 +20,7 @@ export default function Dashboard() {
     {
         carro.allFoods.map((el, id) => {return (
           <div className='rounded-lg relative m-2' key={id}>
-
             <CardAdmin food={el} key={id} />
-
-            <div className='absolute bottom-[15px] left-[800px] z-50 bg-[#971b1b] hover:bg-[#d61313] p-[5px] rounded-lg'>
-              <button className='flex text-s text-center text-[#fff]' >DESACTIVAR PLATO</button>
-            </div>
-            <div className='absolute bottom-[15px] left-[600px] z-50 bg-[#971b1b] hover:bg-[#d61313] p-[5px] rounded-lg'>
-              <button className='flex text-s text-center text-[#fff]' >ACTIVAR PLATO</button>
-            </div>
-
           </div>)})
         }
   </div>
--- client/src/components/admin/CardAdmin.jsx
+++ client/src/components/admin/CardAdmin.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 
 
 //name, image, description, price, review, type, _id
 export default function CardAdmin({food}) {
+
     return (
                 
         <div className="flex h-full" >
-            <div className="flex rounded-lg bg-white shadow-lg container lg:mx-auto border-stone-900 border-2">
+            <div className={`${food.available?'bg-white':'bg-gray-400'} flex rounded-lg shadow-lg container lg:mx-auto border-stone-900 border-2`}>
                 <img className="object-cover sm:w-[90px] sm:h-[90px] md:w-[170px] md:h-[170px] rounded-t-lg md:rounded-none md:rounded-l-lg border-r-2  border-stone-900" src={food.image} alt="imgNotFound" />
                 <div className="p-6 flex flex-col justify-start leading-normal">
                     <h5 className="text-gray-900 text-xl font-medium mb-2">{food.name}</h5>
                 </div>
             </div> 
+            {food.available && <div className='flex items-center justify-center absolute bottom-[15px] left-[600px] w-44 z-50 bg-[#971b1b] hover:bg-[#d61313] p-[5px] rounded-lg'>
+              <button onClick={() => axios.get(`/foods/disableFood/${food._id}`)} className='flex text-s text-center text-[#fff]' >DESACTIVAR PLATO</button>
+            </div>}
+            {!food.available && <div className='flex items-center justify-center absolute bottom-[15px] left-[600px] w-44 z-50 bg-[#36971b] hover:bg-[#41f40f] p-[5px] rounded-lg'>
+              <button onClick={() => axios.get(`/foods/ableFood/${food._id}`)} className='flex text-s text-center text-[#fff]' >ACTIVAR PLATO</button>
+            </div>}
         </div>
     )
 };
