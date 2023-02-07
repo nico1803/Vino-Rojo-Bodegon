993eb8b034fa886ec4d8658144bdf031dcc3793b
Nicolas Beovide <nico1803o@hotmail.com.ar>
2023-02-07 02:48:29 -0300

    Co-authored-by: Alejo Ufano <ufanoalejo@gmail.com>

--- client/src/components/admin/CardAdmin.jsx
+++ client/src/components/admin/CardAdmin.jsx
@@ -5,15 +5,11 @@ import React from 'react';
 export default function CardAdmin({food}) {
     return (
                 
-        <div className="flex bg-white rounded-lg" >
-            <div className="flex container ">
-                <img className=" object-cover sm:w-[90px] sm:h-[90px] md:w-[170px] md:h-[170px] rounded-t-lg md:rounded-none md:rounded-l-lg" src={food.image} alt="imgNotFound" />
+        <div className="flex h-full" >
+            <div className="flex rounded-lg bg-white shadow-lg container lg:mx-auto border-stone-900 border-2">
+                <img className="object-cover sm:w-[90px] sm:h-[90px] md:w-[170px] md:h-[170px] rounded-t-lg md:rounded-none md:rounded-l-lg border-r-2  border-stone-900" src={food.image} alt="imgNotFound" />
                 <div className="p-6 flex flex-col justify-start leading-normal">
                     <h5 className="text-gray-900 text-xl font-medium mb-2">{food.name}</h5>
-
-                    <p className="text-gray-900 text-s">Precio: ${food.price*food.quantity}</p>
-
-                    <p className="text-gray-400 text-s">Cantidad: {food.quantity}</p>
                 </div>
             </div> 
         </div>
