ff5fe3795b1e292c2727642e93255560432ddabd
Eze <ezequicorre@gmail.com>
2023-02-08 11:17:33 -0300

    responsive for filters, iframe and recipesCard fixed

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -14,9 +14,13 @@ export default function Filters() {
     };
 
 
-    const buttonCss = "hover:bg-[#c51b1e] bg-[#720f10] p-3 rounded-full sm:h-[4rem] sm:w-[4rem] md:w-[5rem] md:h-[5rem] lg:w-[7rem] lg:h-[7rem] m-5"
+    const buttonCss = "hover:bg-[#c51b1e] bg-[#720f10] p-3 m-5 rounded-full w-[3rem] h-[3rem] sm:h-[4rem] sm:w-[4rem] md:w-[4rem] md:h-[4rem] lg:w-[5rem] lg:h-[5rem]"
     const SelectedButtonCss = "hover:bg-rose-900 bg-[#720f10] p-3 rounded-full w-[5rem] h-[5rem] m-5 border border-1 border-white"
 
+    const IconCss = "text-[20px] sm:text-[30px] md:text-[30px] lg:text-[40px]"
+    const TextClass = "text-xs sm:text-sm lg:text-lg pt-1 text-gray-200 flex justify-center m-2"
+
+    const divCss = "justify-center bg-slate-900 rounded-lg grid sm:grid-cols-1 md:grid-cols-2 sm:gap-4 md:gap-8 place-items-center h-auto m-5 p-5 animate__animated animate__backInLeft";
     
 
     return (
@@ -25,15 +29,15 @@ export default function Filters() {
         <div className="text-center mt-5">
                 <p className="font-bold text-white">Menu</p>
         </div>
-        <div className="justify-center bg-slate-900 rounded-lg grid sm:grid-cols-1 md:grid-cols-2 sm:gap-4 md:gap-8 place-items-center h-auto m-5 p-5 animate__animated animate__backInLeft" >
+        <div className={divCss} >
 
             <div className={selected===1 ? SelectedButtonCss : buttonCss} onClick={() => handleClick("Entrada",1)}>
 
-                <span className="text-gray-200 flex justify-center absolute">
-                    <i className="ri-knife-line sm:text-[20px] md:text-[40px]"></i>
+                <span className="text-gray-200 flex justify-center">
+                    <i className={"ri-knife-line "+ IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center m-2 absolute">
+                <span className={TextClass}>
                     ENTRADAS
                 </span>
 
@@ -43,10 +47,10 @@ export default function Filters() {
             <div className={selected===2 ? SelectedButtonCss : buttonCss} onClick={() => handleClick("Plato Principal",2)}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-restaurant-2-line text-[40px]"></i>
+                    <i className={"ri-restaurant-2-line "+ IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center m-2">
+                <span className={TextClass}>
                     PRINCIPAL
                 </span>
 
@@ -55,10 +59,10 @@ export default function Filters() {
             <div className={selected===3 ? SelectedButtonCss : buttonCss} onClick={() => handleClick("Guarnicion",3)}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-seedling-line text-[40px]"></i>
+                    <i className={"ri-seedling-line "+  IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center m-2">
+                <span className={TextClass}>
                     GUARNICION
                 </span>
 
@@ -67,10 +71,10 @@ export default function Filters() {
             <div className={buttonCss} onClick={() => handleClick("Postre")}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-trophy-line text-[40px]"></i>
+                    <i className={"ri-trophy-line "+  IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center m-2">
+                <span className={TextClass}>
                     POSTRES
                 </span>
 
@@ -81,17 +85,17 @@ export default function Filters() {
         <div className="text-center mt-5">
             <p className="font-bold text-white">Bebidas</p>
         </div>
-        <div className="justify-center bg-slate-900 rounded-lg grid sm:grid-cols-1 md:grid-cols-3 sm:gap-2 md:gap-4 place-items-center h-auto m-5 p-5 animate__animated  animate__backInLeft">
+        <div className="justify-center bg-slate-900 rounded-lg grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 sm:gap-2 md:gap-4 place-items-center h-auto m-5 p-5 animate__animated  animate__backInLeft">
             
             
 
             <div className={buttonCss} onClick={()=> handleClick("Aguas")}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-goblet-line text-[40px]"></i>
+                    <i className={"ri-goblet-line "+  IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center m-2" >
+                <span className={TextClass} >
                     Aguas
                 </span>
             </div>
@@ -99,10 +103,10 @@ export default function Filters() {
             <div className={buttonCss} onClick={() => handleClick("Gaseosas")}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-goblet-line text-[40px]"></i>
+                    <i className={"ri-goblet-line "+  IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center text-center m-2 leading-[18px]">
+                <span className={TextClass}>
                     Gaseosas
                 </span>
             </div>
@@ -110,10 +114,10 @@ export default function Filters() {
             <div className={buttonCss} onClick={() => handleClick("Cervezas")}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-goblet-line text-[40px]"></i>
+                    <i className={"ri-goblet-line "+  IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center text-center m-2 leading-[18px]">
+                <span className={TextClass}>
                     Cervezas
                 </span>
             </div>
@@ -121,10 +125,10 @@ export default function Filters() {
             <div className={buttonCss} onClick={() => handleClick("Vinos")}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-goblet-line text-[40px]"></i>
+                    <i className={"ri-goblet-line "+  IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center text-center m-2 leading-[18px]">
+                <span className={TextClass}>
                     Vinos
                 </span>
             </div>
@@ -132,10 +136,10 @@ export default function Filters() {
             <div className={buttonCss} onClick={() => handleClick("Tragos")}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-goblet-line text-[40px]"></i>
+                    <i className={"ri-goblet-line "+  IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center text-center m-2 leading-[18px]">
+                <span className={TextClass}>
                     Tragos
                 </span>
             </div>
@@ -152,10 +156,10 @@ export default function Filters() {
             <div className={buttonCss} onClick={null}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-goblet-line text-[40px]"></i>
+                    <i className={"ri-goblet-line "+  IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center m-2">
+                <span className={TextClass}>
                     Mayor Precio
                 </span>
             </div> 
@@ -163,10 +167,10 @@ export default function Filters() {
             <div className={buttonCss} onClick={null}>
 
                 <span className="text-gray-200 flex justify-center">
-                    <i className="ri-filter-off-line text-[40px]"></i>
+                    <i className={"ri-filter-off-line "+  IconCss}></i>
                 </span>
 
-                <span className="text-gray-200 flex justify-center text-center m-2 leading-[18px]">
+                <span className={TextClass}>
                     Menor Precio
                 </span>
             </div>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
 
       <div className='sd:m-5 md:m-10 rounded-lg w-1/4 flex-auto'>
         <Filters/>
-        <div className='sm: flex hidden md:flex '>
+        <div className='flex hidden lg:flex '>
           <iframe title='myFrame' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3320.9652579510075!2d-59.72594984918735!3d-33.658062080620326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95ba1898cde37a09%3A0x5af68ac2a2ffdd61!2sVino%20Rojo!5e0!3m2!1ses-419!2sar!4v1674078948700!5m2!1ses-419!2sar" width="400" height="400" className='mx-auto m-[3em] p-5 rounded-lg' />
         </div>
       </div>
